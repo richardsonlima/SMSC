833666dc42db69a6920affe7fae31c5b6aed5886
Sergej Kunz <s.kunz@bulktrade.org>
2016-05-20 09:09:08 +0200

    some fixes

--- modules/admin/src/app/app.component.ts
+++ modules/admin/src/app/app.component.ts
@@ -57,34 +57,7 @@ import { RouterActive } from './router-active';
       background: #fff;
     }`
     ],
-    template: `
-    <md-content>
-      <md-toolbar color="primary">
-          <span>{{ name }}</span>
-          <span class="fill"></span>
-          <button md-button router-active [routerLink]=" ['Index'] ">
-            Index
-          </button>
-          <button md-button router-active [routerLink]=" ['Home'] ">
-            Home
-          </button>
-          <button md-button router-active [routerLink]=" ['About'] ">
-            About
-          </button>
-      </md-toolbar>
-      
-      <md-progress-bar mode="indeterminate" color="primary" *ngIf="loading"></md-progress-bar>
-
-      <router-outlet></router-outlet>
-
-      <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
-
-      <footer>
-        <img [src]="angularclassLogo" width="6%">
-        WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-      </footer>
-      </md-content>
-  `
+    template: require('./app.html')
 })
 @RouteConfig([
     { path: '/', name: 'Index', component: Home, useAsDefault: true },
@@ -95,8 +68,8 @@ import { RouterActive } from './router-active';
 export class App {
     angularclassLogo = 'assets/img/angularclass-avatar.png';
     loading = false;
-    name = 'Angular 2 Webpack Starter';
-    url = 'https://twitter.com/AngularClass';
+    name = 'SMSC';
+    url = 'http://smsc.io';
 
     constructor(public appState: AppState,
                 public translate: TranslateService) {
